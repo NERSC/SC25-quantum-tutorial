fcbc2b07a8170da2d4855ab1048caa2a4a65fe42
ermalrrapaj <ermalrrapaj@gmail.com>
2025-11-15 11:38:01 -0800

    Update vqe_cudaq_demo.sh

--- Hour2/AFQMC/vqe_cudaq_demo.sh
+++ Hour2/AFQMC/vqe_cudaq_demo.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-#SBATCH -A m4916
+#SBATCH -A ntrain6
 #SBATCH -C gpu
 #SBATCH -q regular
 #SBATCH -t 01:00:00
 #SBATCH -N 1              # Specify number of nodes
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-task=4
+#SBATCH --gpus-per-task=1
 #SBATCH -o vqe_output-%j_%t.out
-
-module load cray-mpich
-module load conda
-conda activate SC_Tutorial_env
+#SBATCH --image=nersc/cudaq:0.12.1
 
 
 # parameter values
@@ -20,7 +17,7 @@ ANSATZ="UCCSD"       # "UCCSD" | "UpCCD"
 INIT="ccsd"          # ccsd | mp2 | zeros
 
 # Launch (one MPI rank per node; each rank sees 4 GPUs)
-srun python vqe_cudaq_demo.py \
+srun -n 1 -cpu-bind=cores --gpu-bind=none --mpi=pmix --module cuda-mpich shifter python vqe_cudaq_demo.py \
   --total-h-atoms ${TOTAL_H_ATOMS} \
   --h-spacing ${H_SPACING} \
   --ansatz ${ANSATZ} \
